e2bf5ce391674f694d6eaa9b14afa904feff173e
Karrick S. McDermott <karrick@karrick.net>
2018-02-28 02:18:20 -0500

    refactor

--- main.go
+++ main.go
@@ -36,12 +36,6 @@ func main() {
 	}
 }
 
-func verbose(format string, a ...interface{}) {
-	if *optVerbose {
-		_, _ = fmt.Fprintf(os.Stderr, format, a...)
-	}
-}
-
 func exit(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
@@ -55,69 +49,102 @@ func usage(message string) {
 	os.Exit(2)
 }
 
-func receive(operands []string) error {
-	if len(operands) < 1 {
-		usage(fmt.Sprintf("cannot receive without binding address"))
+func verbose(format string, a ...interface{}) {
+	if *optVerbose {
+		_, _ = fmt.Fprintf(os.Stderr, format, a...)
 	}
-	l, err := net.Listen("tcp", operands[0])
+}
+
+func withGzipReader(use bool, ior io.Reader, callback func(ior io.Reader) error) error {
+	if !use {
+		return callback(ior)
+	}
+	verbose("# Using gzip compression\n")
+	z, err := gzip.NewReader(ior)
 	if err != nil {
 		return err
 	}
-	verbose("# Listening: %q\n", operands[0])
-	conn, err := l.Accept()
+	err = callback(z)
+	if err2 := z.Close(); err == nil {
+		err = err2
+	}
+	return err
+}
+
+func withGzipWriter(use bool, iow io.Writer, callback func(iow io.Writer) error) error {
+	if !use {
+		return callback(iow)
+	}
+	verbose("# Using gzip compression\n")
+	z := gzip.NewWriter(iow)
+	err := callback(z)
+	if err2 := z.Close(); err == nil {
+		err = err2
+	}
+	return err
+}
+
+func withDial(remote string, callback func(iow io.Writer) error) error {
+	conn, err := net.Dial("tcp", remote)
 	if err != nil {
 		return err
 	}
-	verbose("# Accepted connection: %q\n", conn.RemoteAddr())
-	var ior io.Reader = conn
-	if *optZip {
-		verbose("# Using gzip compression\n")
-		ior, err = gzip.NewReader(ior)
-		if err != nil {
-			_ = conn.Close()
-			return err
-		}
-	}
-	cr, err := io.Copy(os.Stdout, ior)
-	if cr > 0 {
-		verbose("# Received %d bytes\n", cr)
-	}
-	if *optZip {
-		if err2 := ior.(*gzip.Reader).Close(); err == nil {
-			err = err2
-		}
-	}
+	verbose("# Connected: %q\n", conn.RemoteAddr())
+
+	err = withGzipWriter(*optZip, conn, func(iow io.Writer) error {
+		return callback(iow)
+	})
+
 	if err2 := conn.Close(); err == nil {
 		err = err2
 	}
 	return err
 }
 
-func send(operands []string) error {
-	if len(operands) < 1 {
-		usage(fmt.Sprintf("cannot send without destination address"))
-	}
-	conn, err := net.Dial("tcp", operands[0])
+func withListen(bind string, callback func(ior io.Reader) error) error {
+	l, err := net.Listen("tcp", bind)
 	if err != nil {
 		return err
 	}
-	verbose("# Connected: %q\n", conn.RemoteAddr())
-	var iow io.Writer = conn
-	if *optZip {
-		verbose("# Using gzip compression\n")
-		iow = gzip.NewWriter(iow)
+	verbose("# Listening: %q\n", bind)
+	conn, err := l.Accept()
+	if err != nil {
+		return err
 	}
-	cr, err := io.Copy(iow, os.Stdin)
-	if cr > 0 {
-		verbose("# Sent %d bytes\n", cr)
-	}
-	if *optZip {
-		if err2 := iow.(*gzip.Writer).Close(); err == nil {
-			err = err2
-		}
-	}
+	verbose("# Accepted connection: %q\n", conn.RemoteAddr())
+
+	err = withGzipReader(*optZip, conn, func(ior io.Reader) error {
+		return callback(ior)
+	})
+
 	if err2 := conn.Close(); err == nil {
 		err = err2
 	}
 	return err
+}
+
+func receive(operands []string) error {
+	if len(operands) < 1 {
+		usage(fmt.Sprintf("cannot receive without binding address"))
+	}
+	return withListen(operands[0], func(ior io.Reader) error {
+		cr, err := io.Copy(os.Stdout, ior)
+		if cr > 0 {
+			verbose("# Received %d bytes\n", cr)
+		}
+		return err
+	})
+}
+
+func send(operands []string) error {
+	if len(operands) < 1 {
+		usage(fmt.Sprintf("cannot send without destination address"))
+	}
+	return withDial(operands[0], func(iow io.Writer) error {
+		cr, err := io.Copy(iow, os.Stdin)
+		if cr > 0 {
+			verbose("# Sent %d bytes\n", cr)
+		}
+		return err
+	})
 }
